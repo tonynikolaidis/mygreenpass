73649a47325e1fce276bd0f4f94f9b0e584510b2
Anthony Nikolaidis <a.nikolaidis@student.tudelft.nl>
2022-01-03 00:47:43 +0200

    Add the ability to view test and recovery certificate details

--- lib/certificate_details_page.dart
+++ lib/certificate_details_page.dart
@@ -1,24 +1,32 @@
 import 'package:country_codes/country_codes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mycovidpass/utils/navigation_bar_color.dart';
+import 'package:mycovidpass/utils/recovery_details.dart';
+import 'package:mycovidpass/utils/test_details.dart';
+import 'package:mycovidpass/utils/vaccination_details.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'certificates_page.dart';
 import 'get_started_page.dart';
+import 'main.dart';
 import 'utils/call_to_action_button.dart';
 import 'utils/constants.dart';
 import 'utils/qr_scanner.dart';
 import 'utils/storage.dart';
 import 'utils/white_background_painter.dart';
 
-class CertificateDetailsPage extends StatefulWidget {
+import 'package:flutter_linkify/flutter_linkify.dart';
+
+class CertificateDetailsPage extends ConsumerWidget {
   final title;
   final code;
   final data;
   final bool save;
 
-  const CertificateDetailsPage(
+  CertificateDetailsPage(
       {Key? key,
       required this.title,
       required this.code,
@@ -27,33 +35,25 @@ class CertificateDetailsPage extends StatefulWidget {
       : super(key: key);
 
   @override
-  _CertificateDetailsPageState createState() => _CertificateDetailsPageState();
-}
-
-class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
-  bool saveButtonIsDisabled = false;
-  late Widget button;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    late Widget button;
     NavigationBarColor.changeTo('white');
 
     List<String>? codes = Storage.getItem('codes');
-    setState(() {
-      saveButtonIsDisabled = codes!.contains(widget.code);
-    });
 
-    if (widget.save) {
+    if (save) {
       button = CallToActionButton(
           extended: true,
-          disabled: saveButtonIsDisabled,
+          disabled: codes!.contains(code),
           heroTag: 'save',
           label: 'Save',
           icon: Icon(Icons.check_rounded),
           onPressed: () async {
-            if (!codes!.contains(widget.code)) {
-              codes.add(widget.code);
+            if (!codes.contains(code)) {
+              codes.add(code);
               await Storage.setItem('codes', codes);
+
+              ref.refresh(certificates);
 
               Navigator.pushAndRemoveUntil(
                   context,
@@ -70,28 +70,84 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
           heroTag: 'delete',
           label: 'Delete',
           icon: Icon(Icons.clear_rounded),
-          onPressed: () async {
-            List<String>? codes = Storage.getItem('codes');
+          onPressed: () => {
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: Text(
+                      'Delete certificate',
+                      style: TextStyle(color: COLOR_BLUE, fontSize: 24.0),
+                    ),
+                    content: Text(
+                      'Are you sure you want to delete this certificate?',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    backgroundColor: Colors.white,
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        style: ButtonStyle(
+                          overlayColor: MaterialStateColor.resolveWith(
+                              (states) => COLOR_BLACK_SPLASH),
+                        ),
+                        child: Text('Cancel',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0)),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          List<String>? codes = Storage.getItem('codes');
 
-            if (codes!.contains(widget.code)) {
-              codes.remove(widget.code);
-              await Storage.setItem('codes', codes);
+                          if (codes!.contains(code)) {
+                            codes.remove(code);
+                            await Storage.setItem('codes', codes);
 
-              if (codes.length == 0) {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => GetStartedPage()),
-                    (route) => route.isCurrent);
+                            if (codes.length == 0) {
+                              Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => GetStartedPage()),
+                                  (route) => route.isCurrent);
 
-                NavigationBarColor.changeTo('blue');
-              } else {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => CertificatesPage()),
-                    (route) => route.isCurrent);
-              }
-            }
-          });
+                              NavigationBarColor.changeTo('blue');
+                            } else {
+                              // Navigator.pushAndRemoveUntil(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => CertificatesPage()),
+                              //     (route) => route.isCurrent);
+
+                              Navigator.pop(context);
+                              Navigator.pop(context);
+                            }
+
+                            ref.refresh(certificates);
+                          }
+                        },
+                        style: ButtonStyle(
+                          overlayColor: MaterialStateColor.resolveWith(
+                              (states) => COLOR_RED_SPLASH_LIGHT),
+                        ),
+                        child: Text('Delete',
+                            style: TextStyle(color: COLOR_RED, fontSize: 16.0)),
+                      ),
+                    ],
+                  ),
+                )
+              });
+    }
+
+    String header = 'EMPTY';
+    var scaleFactor = 1.0;
+
+    if (data.containsKey('v')) {
+      header = 'VACCINATION';
+    } else if (data.containsKey('t')) {
+      header = 'TEST';
+      scaleFactor = 1.05;
+    } else if (data.containsKey('r')) {
+      header = 'RECOVERY';
+      scaleFactor = 1.05;
     }
 
     return WillPopScope(
@@ -104,7 +160,7 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
       child: Scaffold(
         appBar: AppBar(
           title: Text(
-            widget.title,
+            title,
             style: TextStyle(color: Colors.black),
           ),
           elevation: 0,
@@ -140,25 +196,32 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
                       SizedBox(
                         height: 10,
                       ),
-                      QrImage(
-                        data: widget.code,
-                        version: QrVersions.auto,
-                        size: 200,
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Transform.scale(
+                          scale: scaleFactor,
+                          child: QrImage(
+                            data: code,
+                            version: QrVersions.auto,
+                            size: 170,
+                            padding: const EdgeInsets.all(0.0),
+                          ),
+                        ),
                       ),
                       SizedBox(
                         height: 10,
                       ),
                       Text(
-                        widget.data['lastNameT'] +
+                        data['lastNameT'] +
                             ' ' +
-                            widget.data['firstNameT'],
+                            data['firstNameT'],
                         style: TextStyle(
                             fontSize: 24,
                             fontWeight: FontWeight.w500,
                             color: COLOR_BLUE),
                       ),
                       Text(
-                        widget.data['dateOfBirth'],
+                        data['dateOfBirth'],
                         style: TextStyle(
                           fontSize: 18,
                         ),
@@ -175,7 +238,7 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
                               children: [
                                 SizedBox(height: 15),
                                 Text(
-                                  'VACCINATION CERTIFICATE',
+                                  header + ' CERTIFICATE',
                                   style: TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.w600,
@@ -191,51 +254,49 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
                       SizedBox(
                         height: 20,
                       ),
-                      CertificateInfo(
-                        header: 'DISEASE OR AGENT TARGETED',
-                        body: widget.data['v']['tg'],
-                      ),
-                      CertificateInfo(
-                        header: 'COVID-19 VACCINE OR PROPHYLAXIS',
-                        body: widget.data['v']['vp'],
-                      ),
-                      CertificateInfo(
-                        header: 'COVID-19 VACCINE MEDICINAL PRODUCT',
-                        body: widget.data['v']['mp'],
-                      ),
-                      CertificateInfo(
-                        header:
-                            'COVID-19 VACCINE MARKETING AUTHORISATION HOLDER OR MANUFACTURER',
-                        body: widget.data['v']['ma'],
-                      ),
-                      CertificateInfo(
-                        header: 'DOSES',
-                        body:
-                            widget.data['v']['dn'] + '/' + widget.data['v']['sd'],
-                      ),
-                      CertificateInfo(
-                        header: 'DATE OF VACCINATION',
-                        body: widget.data['v']['dt'],
-                      ),
-                      CertificateInfo(
-                        header:
-                            'MEMBER STATE IN WHICH THE VACCINE WAS ADMINISTERED',
-                        body: CountryCodes.detailsForLocale(Locale.fromSubtags(
-                                countryCode: widget.data['v']['co']))
-                            .localizedName
-                            .toString(),
-                      ),
-                      CertificateInfo(
-                        header: 'CERTIFICATE ISSUER',
-                        body: widget.data['v']['iss'],
-                      ),
-                      CertificateInfo(
-                        header: 'UNIQUE CERTIFICATE IDENTIFIER',
-                        body: widget.data['v']['ci'],
+                      buildCertificateDetails(data),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 90 / 100,
+                        decoration: BoxDecoration(
+                            color: COLOR_GLASS_PURPLE,
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        child: Padding(
+                          padding: EdgeInsets.all(
+                              MediaQuery.of(context).size.width * 5 / 100),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  'This certificate is not a travel document.\n',
+                                  style: TextStyle(fontSize: 16)),
+                              Text(
+                                  'The scientific evidence on COVID-19 vaccination, testing and recovery continues to evolve, including with regard to new virus variants of concern.\n',
+                                  style: TextStyle(fontSize: 16)),
+                              Text(
+                                  'Before travelling, please check the applicable public health measure and related restrictions applicable at the point of destination.\n',
+                                  style: TextStyle(fontSize: 16)),
+                              Linkify(
+                                onOpen: (link) async {
+                                  if (await canLaunch(link.url)) {
+                                    await launch(link.url);
+                                  } else {
+                                    throw 'Could not launch $link';
+                                  }
+                                },
+                                text: "Relevant information can be found here:\nhttps://reopen.europa.eu/en",
+                                style: TextStyle(fontSize: 16),
+                                linkStyle: TextStyle(fontSize: 16, color: COLOR_BLUE),
+                                options: LinkifyOptions(humanize: false),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                       SizedBox(
                         height: 60,
-                      )
+                      ),
+                      SizedBox(height: 32 // bottomOffset,
+                          ),
                     ],
                   )
                 ],
@@ -245,5 +306,19 @@ class _CertificateDetailsPageState extends State<CertificateDetailsPage> {
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
     );
+  }
+
+  Widget buildCertificateDetails(Map data) {
+    if (data.containsKey('v')) {
+      return VaccinationDetails(data: data);
+
+    } else if (data.containsKey('t')) {
+      return TestDetails(data: data);
+
+    } else if (data.containsKey('r')) {
+      return RecoveryDetails(data: data);
+    }
+
+    return Column();
   }
 }
