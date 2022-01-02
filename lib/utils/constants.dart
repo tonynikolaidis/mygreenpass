cd1945a2d111e6cb856f2ccf73c28e24575b6735
Anthony Nikolaidis <a.nikolaidis@student.tudelft.nl>
2022-01-03 00:44:05 +0200

    Add colours

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,24 +3,29 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const COLOR_BLUE = Color.fromRGBO(0, 51, 153, 1.0);
-const COLOR_BLUE_SPLASH = Color.fromRGBO(31, 71, 177, 0);
+const COLOR_BLUE_SPLASH = Color.fromRGBO(17, 61, 165, 1.0); // 31, 71, 177
+const COLOR_BLUE_SPLASH_LIGHT = Color.fromRGBO(231, 234, 247, 1.0); // 194, 201, 234,
+
 const COLOR_YELLOW = Color.fromRGBO(255, 204, 0, 1.0);
 // const COLOR_YELLOW_SPLASH = Color.fromRGBO(255, 220, 82, 1.0);
-const COLOR_YELLOW_SPLASH = Color.fromRGBO(255, 255, 80, 1.0);
+const COLOR_YELLOW_SPLASH = Color.fromRGBO(255, 215, 67, 1.0); // 255, 255, 80
 
 const COLOR_GRAY = Color.fromRGBO(186, 186, 186, 1.0);
 const COLOR_GRAY_SPLASH = Color.fromRGBO(236, 236, 236, 1.0);
 
 // const COLOR_RED = Color.fromRGBO(255, 0, 51, 1);
 const COLOR_RED = Color.fromRGBO(255, 29, 52, 1);
-const COLOR_RED_SPLASH = Color.fromRGBO(255, 99, 95, 1.0);
+const COLOR_RED_SPLASH = Color.fromRGBO(249, 62, 79, 1.0); // 255, 99, 95
+const COLOR_RED_SPLASH_LIGHT = Color.fromRGBO(255, 234, 238, 1.0); // 255, 202, 211,
 
 const COLOR_GLASS_WHITE = Color.fromRGBO(255, 255, 255, 0.45);
+const COLOR_GLASS_WHITE_2 = Color.fromRGBO(255, 255, 255, 0.75);
 const COLOR_GLASS_PURPLE = Color.fromRGBO(209, 225, 255, 0.45);
 const COLOR_BORDER_WHITE = Color.fromRGBO(255, 255, 255, 0.15);
 const COLOR_BORDER_PURPLE = Color.fromRGBO(209, 225, 255, 0.1);
 
 const COLOR_BLACK = Color.fromRGBO(0, 0, 0, 1.0);
+const COLOR_BLACK_SPLASH = Color.fromRGBO(233, 233, 233, 1.0);
 
 const GRADIENT_GLASS = RadialGradient(
     center: Alignment.bottomRight,
